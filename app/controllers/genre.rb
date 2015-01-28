ff077362e2ab5b424603aa11e0ea4598baa9e6d6
Daniella Shin <thedanpan@gmail.com>
2015-01-28 11:49:46 -0500

    add route handler for single genre by id

--- app/controllers/genre.rb
+++ app/controllers/genre.rb
@@ -2,3 +2,9 @@ get '/genre/all' do
   genres = Genre.all
   erb :'genres/all', locals: {genres: genres}
 end
+
+
+get '/genre/:id' do |id|
+  genre = Genre.find(id)
+  erb :'genres/show', locals: {genre: genre}
+end
