966e093eb08e7794211f00d1af0ba4d27492e294
Daniella Shin <thedanpan@gmail.com>
2015-01-28 15:19:01 -0500

    add comments table migration

--- db/migrate/20150128144847_create_comments.rb
+++ db/migrate/20150128144847_create_comments.rb
@@ -1,4 +1,11 @@
 class CreateComments < ActiveRecord::Migration
   def change
+    create_table :comments do |t|
+      t.text :content
+
+      t.references :story
+
+      t.timestamps null: true
+    end
   end
 end
