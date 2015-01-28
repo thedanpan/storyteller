30005837ff5abdd68ac58d95741b0907a793ff84
Daniella Shin <thedanpan@gmail.com>
2015-01-28 11:32:42 -0500

    add route handler to single story by id

--- app/controllers/story.rb
+++ app/controllers/story.rb
@@ -2,3 +2,9 @@ get '/story/all' do
   stories = Story.all
   erb :'stories/all', locals: {stories: stories}
 end
+
+
+get '/story/:id' do |id|
+  story = Story.find(id)
+  erb :'stories/show', locals: {story: story}
+end
