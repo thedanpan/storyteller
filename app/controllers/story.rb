9644d5129b22afa11ada1925f19be0e8436c1385
Daniella Shin <thedanpan@gmail.com>
2015-01-28 14:31:52 -0500

    add route to handle new stories

--- app/controllers/story.rb
+++ app/controllers/story.rb
@@ -1,3 +1,18 @@
+get '/story/new' do
+  genres = Genre.all
+  erb :'stories/new', locals: {genres: genres}
+end
+
+# try to refactor this
+post '/story/new', auth: :user do
+  story = current_user.stories.create(
+    title: params[:story][:title],
+    content: params[:story][:content],
+    fiction: params[:story][:fiction],
+    genre: Genre.find_by(name: params[:story][:genre_name]) )
+  redirect '/story/all/user'
+end
+
 get '/story/all' do
   stories = Story.all
   erb :'stories/all', locals: {stories: stories}
