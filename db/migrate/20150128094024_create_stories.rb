9233141e98ac06771f6d9d9dd0ffa3d3a39e1077
Daniella Shin <thedanpan@gmail.com>
2015-01-28 09:46:48 -0500

    create table stories

--- db/migrate/20150128094024_create_stories.rb
+++ db/migrate/20150128094024_create_stories.rb
@@ -1,4 +1,14 @@
 class CreateStories < ActiveRecord::Migration
   def change
+    create_table :stories do |t|
+      t.string :author
+      t.string :genre
+
+      t.string :title
+      t.text :content
+      t.boolean :fiction
+
+      t.timestamps null: true
+    end
   end
 end
