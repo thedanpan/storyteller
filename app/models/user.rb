9891b2aa12ff7d4f81d061e52cd853455f1e221d
Daniella Shin <thedanpan@gmail.com>
2015-01-28 10:01:57 -0500

    create associations for user to authored stories with email validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  has_secure_password
+
+  has_many :stories, class_name: "Story", foreign_key: :author_id
+
+  validates :email, :presence => {:message => "Must provide an email"}
+  validates :email, :uniqueness => {:message => "Sorry, that email is already taken"}
 end
