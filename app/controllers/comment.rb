02aafb1f629ed2818fac2e6f74fd57c9c4923f0e
Daniella Shin <thedanpan@gmail.com>
2015-01-28 19:00:41 -0500

    add new comment route handler

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -1,11 +1,15 @@
 get '/comment/new' do
-  erb :'/comment/new'
+  erb :'/comments/new'
 end
 
-post '/comment/new', auth: :user do
-  params[:comment][:commentor] = current_user
-  comment = Comment.new(params[:comment])
+post '/comment/new' do
+  comment = Comment.create(
+    content: params[:comment][:content],
+    story_id: params[:comment][:story_id],
+    commentor_id: params[:comment][:commentor_id]
+    )
   unless comment.save
     set_error!("Something went wrong while saving your comment. Please try again.")
   end
+  redirect "/story/#{comment.story_id}"
 end
