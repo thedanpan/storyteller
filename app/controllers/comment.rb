82f52b671ca1aabd4181a6a43e7678760c5c0d3b
Daniella Shin <thedanpan@gmail.com>
2015-01-28 19:01:03 -0500

    add edit and delete route handlers for comments

--- app/controllers/comment.rb
+++ app/controllers/comment.rb
@@ -13,3 +13,34 @@ post '/comment/new' do
   end
   redirect "/story/#{comment.story_id}"
 end
+
+get '/comment/:id' do
+  comment = Comment.find(id)
+  erb :'comments/show', locals: {comment: comment}
+end
+
+
+get '/comment/:id/edit', auth: :user do |id|
+  comment = Comment.find(id)
+  erb :'comments/edit', locals: {comment: comment}
+end
+
+
+put '/comment/:id/edit', auth: :user do |id|
+  comment = Comment.find(id)
+  comment.update(content: params[:comment][:content])
+  redirect "/story/#{comment.story_id}"
+end
+
+
+delete '/comment/:id', auth: :user do |id|
+  comment = Comment.find(id)
+  if commentor?(comment)
+    comment.destroy
+  else
+    set_error!("Sorry, you don't have permission to delete this comment.")
+  end
+  redirect "/story/#{comment.story_id}"
+end
+
+
